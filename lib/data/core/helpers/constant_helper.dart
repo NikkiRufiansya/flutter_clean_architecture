2bad469fb018b0ebef45da35f937040617bed152
alimarief <alimarief97@gmail.com>
2023-06-27 21:54:31 +0700

    Added dio exception & constant helper

---
+++ lib/data/core/helpers/constant_helper.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: non_constant_identifier_names
+
+class ConstantHelper {
+  static String API_KEY = "2802c6db7bbd672f9d65f133b2494ac3";
+  static String BASE_URL = 'https://api.themoviedb.org/3/movie';
+
+
+  static String NOW_PLAYING = '/now_playing?language=en-US&page=1';
+  static String POPULAR = 'popular/?language=en-US&page=1';
+  static String UPCOMING = '/upcoming?language=en-US&page=1';
+}
